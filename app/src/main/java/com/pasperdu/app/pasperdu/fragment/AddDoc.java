87215ad026b4a5cd0bb92c39ceedeb8286483582
julio.fils1@gmail.com <julio.fils1@gmail.com>
2017-04-21 19:43:44 -0400

    First commit

---
+++ app/src/main/java/com/pasperdu/app/pasperdu/fragment/AddDoc.java
@@ -0,0 +1,28 @@
+package com.pasperdu.app.pasperdu.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pasperdu.app.pasperdu.R;
+
+public class AddDoc extends Fragment {
+    public AddDoc() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_doc, container, false);
+    }
+
+}
